d59f5f98b90c982895bcdd5f86d5b6b50563244e
Samuel Avella <samu.av.jm18@gmail.com>
2024-12-10 14:35:07 +0100

    first commit

---
+++ Ejercicio3/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+
+            MessageBox.Show($"Ancho: {button.Width}, Alto: {button.Height}", "Información del botón");
+        }
+    }
+}
---
+++ Ejercicio5/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio5
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cb_TodosHaCambiado(object sender, RoutedEventArgs e)
+        {
+            bool nuevoValor = (cbTodos.IsChecked == true);
+
+            cbDI.IsChecked = nuevoValor;
+            cbSGE.IsChecked = nuevoValor;
+            cbAD.IsChecked = nuevoValor;
+        }
+        private void cb_SimpleHaCambiado(object sender, RoutedEventArgs e)
+        {
+            cbTodos.IsChecked = null;
+            if ((cbDI.IsChecked == true) && (cbSGE.IsChecked == true) && (cbAD.IsChecked == true))
+            {
+                cbTodos.IsChecked = true;
+            }
+            else if ((cbDI.IsChecked == false) && (cbSGE.IsChecked == false) && (cbAD.IsChecked == false))
+            {
+                cbTodos.IsChecked = false;
+            }
+
+        }
+        }
+    }
